b6fd234212df6bb3da8502329469e9acc1f7056b
MoyosoreCoder <kelanimaryam1234@gmail.com>
2023-10-16 16:05:32 -0700

    new update

--- _printf.c
+++ _printf.c
@@ -9,63 +9,33 @@
 int _printf(const char *format, ...)
 {
 	va_list options;
-	int count = 0, i;
+	int count = 0;
 
 	va_start(options, format);
 
 	while (*format)
 	{
-		if (*format != '%')
-		{
-			_putchar(*format);
-			couunt++;
-		}
-		else
+		if (*format == '%')
 		{
 			format++;
-			count += process_format_spe(&format, options);
-		}
-		format++;
-	}
-	va_end (options);
-	return (count - 1);
-}
+
+			if (*format == '\0')
+			{
+				break;
+			}
+			switch (*format)
+			{
+
 /**
  * print_ch - prints a chaarcter
  * @options: a va_list containing charcter to print
  *
  * Return: the number of characters to ptint
  */
-int print_ch(va_list options)
-{
-	char c;
 
-	c = va_arg(options, int);
-
-	return (_putchar(c));
-}
 /**
  * print_str -  a function that prints a string
  * @options: a va_list containing strings to print
  *
- * Return: the number of characters printed
+ * Return: the number of character
  */
-int print_str(va_list options)
-{
-	char *str;
-	int count = 0;
-
-	str = va_arg(options, char *);
-
-	if (str == NULL)
-	{
-		return (0);
-	}
-
-	while (*str)
-	{
-		count += _putchar(*str);
-		str++;
-	}
-	return (count);
-}
