74144de5063535c1d46d27d808104f6ba53c663e
MoyosoreCoder <kelanimaryam1234@gmail.com>
2023-10-17 01:17:44 -0700

    updateded _printf

--- _printf.c
+++ _printf.c
@@ -1,29 +1,38 @@
 #include "main.h"
 /**
- * print_ch - print a character
- * @options: va_list that contain the character
+ * _putchar - a fuction that writes a character to stdout
+ * @c: the character to write
+ * Return: on success, the number of character written
+ * On error, -1 is returrned, errno is set appropriately
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+/**
+ * print_ch - function that prints character
+ * @options: a va_list containing arguments
  *
- * Return: the number of characters
+ * Return: the number of charcters
  */
 int print_ch(va_list options)
 {
 	char c;
-
 	c = va_arg(options, int);
 
 	return (_putchar(c));
 }
 /**
- * print_str - prints a string
- * @options: va_list that contain the string
+ * print_str - a function that prints a string
+ * @options: a va_list containing arguments
  *
- * Return: number of string
+ * Return: the numbers of characters
  */
 int print_str(va_list options)
 {
-	int ch_count = 0;
-
 	char *str;
+
+	int ch_count = 0;
 
 	str = va_arg(options, char *);
 
@@ -35,26 +44,23 @@ int print_str(va_list options)
 	return (ch_count);
 }
 /**
- * print_percent - function that prints percent
- *
- * Return: the number of character printed in an array
+ * print_percent - prints percent
+ * Return: the numbers of charcters printed on the screen
  */
 int print_percent(void)
 {
 	return (_putchar('%'));
 }
 /**
- * _printf - this is a function that formatted string
- * @format: a format string containing format specifier
- *
- * Return: The number of charcaters printed
+ * _printf - function that prints formatted string
+ * @format: arguments containing different format specifier
+ * Return: the number of characters printed excluding the null byte
  */
 int _printf(const char *format, ...)
 {
-	va_list options;
-
 	int ch_count = 0;
 
+	va_list options;
 	va_start(options, format);
 
 	while (*format)
@@ -62,6 +68,7 @@ int _printf(const char *format, ...)
 		if (*format == '%')
 		{
 			format++;
+
 			if (*format == '\0')
 			{
 				break;
@@ -85,7 +92,7 @@ int _printf(const char *format, ...)
 		}
 		else
 		{
-			ch_count = _putchar(*format);
+			ch_count += _putchar(*format);
 		}
 		format++;
 	}
--- main.h
+++ main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <unistd.h>
 #include <stddef.h>
 #include <stdarg.h>
 int _printf(const char *format, ...);
